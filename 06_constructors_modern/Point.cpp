77aa323b00d3489a6c0293c01256ab91b35a60b7
Marwah <marwah.prog@gmail.com>
2026-09-13 21:50:57 -0700

    Set up fork and updated bootcamp tasks

--- 06_constructors_modern/Point.cpp
+++ 06_constructors_modern/Point.cpp
@@ -13,8 +13,9 @@ Point::Point(double x, double y):x_(x),y_(y) {
 
 }
 
-Point::Point(double xy): Point(xy, xy){
+Point::Point(double xy): Point(xy, xy) {
     // TODO: delegate to the two-argument constructor with both
     // coordinates set to xy. Rewrite the line above to read:
     //   Point::Point(double xy) : Point(xy, xy) {
+
 }
